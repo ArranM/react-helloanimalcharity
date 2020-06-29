d3e9b84961766f052a99cdb7453bcb3ed719c09f
ArranM <arran@web-site.co.uk>
2020-06-29 13:46:26 +0100

    added semantic-ui-react to the project

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import { Button } from 'semantic-ui-react';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>Hello Animal Charity</h1>
+      <div className='app'>
+        <h1>Hello Animal Charity!</h1>
+        <Button icon="smile" content="React Button" />
       </div>
     );
   }
