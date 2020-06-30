9d76a9a83e20d5c452db88f64949bbd1ee2888af
ArranM <arran@web-site.co.uk>
2020-06-30 15:46:36 +0100

    end of section basic form with add to events list in dashboard page, passed from child to partent

--- src/features/event/EventForm/EventForm.jsx
+++ src/features/event/EventForm/EventForm.jsx
@@ -12,7 +12,7 @@ class EventForm extends Component {
 
     handleFromSubmit = (evt) => {
         evt.preventDefault();
-        console.log(this.state)
+        this.props.createEvent(this.state);
     }
 
     handleInputChange = ({ target: { name, value } }) => {
