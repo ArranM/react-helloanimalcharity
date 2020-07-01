a0cf8ba20efdd817a4b50c57320e77375d9d20f5
ArranM <arran@web-site.co.uk>
2020-07-01 11:02:22 +0100

    create read and delete basic added for front page - does return on F5 as no backend yet

--- src/features/event/EventDashboard/EventDashboard.jsx
+++ src/features/event/EventDashboard/EventDashboard.jsx
@@ -8,7 +8,7 @@ const events = [
     {
         id: '1',
         title: 'Trip to Tower of London',
-        date: '2018-03-27T11:00:00+00:00',
+        date: '2018-03-27',
         category: 'culture',
         description:
             'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sollicitudin ligula eu leo tincidunt, quis scelerisque magna dapibus. Sed eget ipsum vel arcu vehicula ullamcorper.',
@@ -32,7 +32,7 @@ const events = [
     {
         id: '2',
         title: 'Trip to Punch and Judy Pub',
-        date: '2018-03-28T14:00:00+00:00',
+        date: '2018-03-28',
         category: 'drinks',
         description:
             'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sollicitudin ligula eu leo tincidunt, quis scelerisque magna dapibus. Sed eget ipsum vel arcu vehicula ullamcorper.',
@@ -59,14 +59,28 @@ const events = [
 class EventDashboard extends Component {
     state = {
         events: events,
-        isOpen: false
+        isOpen: false,
+        selectedEvent: null
     }
 
-    handleIsOpenToggle = () => {
-        this.setState(({ isOpen }) => ({
-            isOpen: !isOpen
-        }))
+
+    handleCreateFromOpen = () => {
+        this.setState({
+            isOpen: true,
+            selectedEvent: null
+        })
     }
+
+    handleCreateFromCancel = () => {
+        this.setState({
+            isOpen: false
+        })
+    }
+    // handleIsOpenToggle = () => {
+    //     this.setState(({ isOpen }) => ({
+    //         isOpen: !isOpen
+    //     }))
+    // }
 
     handleCreateEvent = (newEvent) => {
         newEvent.id = cuid();
@@ -77,17 +91,44 @@ class EventDashboard extends Component {
         }));
     }
 
+    handleSelectEvent = (event) => {
+        this.setState({
+            selectedEvent: event,
+            isOpen: true
+        })
+    }
+
+    handleUpdateEvent = (updatedEvent) => {
+        this.setState(({ events }) => ({
+            events: events.map(event => {
+                if (event.id === updatedEvent.id) {
+                    return { ...updatedEvent }
+                } else {
+                    return event
+                }
+            }),
+            isOpen : false,
+            selectedEvent : null
+        }))
+    }
+
+    handleDeleteEvent = (id) => {
+        this.setState(({events}) => ({
+            events: events.filter(e => e.id !== id)
+        }))
+    }
+
     render() {
-        const { events, isOpen } = this.state;
+        const { events, isOpen, selectedEvent } = this.state;
 
         return (
             <Grid>
                 <Grid.Column width={10}>
-                    <EventList events={events} />
+                    <EventList deleteEvent={this.handleDeleteEvent} selectEvent={this.handleSelectEvent} events={events} />
                 </Grid.Column>
                 <Grid.Column width={6}>
-                    <Button onClick={this.handleIsOpenToggle} positive content='Create Event' />
-                    {isOpen && <EventForm createEvent={this.handleCreateEvent} cancelFormOpen={this.handleIsOpenToggle} />}
+                    <Button onClick={this.handleCreateFromOpen} positive content='Create Event' />
+                    {isOpen && <EventForm key={selectedEvent ? selectedEvent.id : 0} updateEvent={this.handleUpdateEvent} selectedEvent={selectedEvent} createEvent={this.handleCreateEvent} cancelFormOpen={this.handleCreateFromCancel} />}
 
                 </Grid.Column>
             </Grid>
